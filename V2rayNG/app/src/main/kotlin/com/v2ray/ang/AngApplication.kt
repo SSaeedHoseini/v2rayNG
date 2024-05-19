bf1b55200a96627f82c621aa51256b73b4c5890f
ssaeedhoseini <hoseini.py@gmail.com>
2024-05-19 12:24:16 +0330

    feture: load direct and block routing

--- V2rayNG/app/src/main/kotlin/com/v2ray/ang/AngApplication.kt
+++ V2rayNG/app/src/main/kotlin/com/v2ray/ang/AngApplication.kt
@@ -4,9 +4,12 @@ import android.content.Context
 import androidx.multidex.MultiDexApplication
 import androidx.work.Configuration
 import com.tencent.mmkv.MMKV
+import com.v2ray.ang.util.MmkvManager
 import com.v2ray.ang.util.Utils
 
 class AngApplication : MultiDexApplication(), Configuration.Provider {
+    private val settingsStorage by lazy { MMKV.mmkvWithID(MmkvManager.ID_SETTING, MMKV.MULTI_PROCESS_MODE) }
+
     companion object {
         //const val PREF_LAST_VERSION = "pref_last_version"
         lateinit var application: AngApplication
@@ -34,11 +37,31 @@ class AngApplication : MultiDexApplication(), Configuration.Provider {
         MMKV.initialize(this)
 
         Utils.setNightMode(application)
+
+        loadDataToPreference(
+            AppConfig.PREF_V2RAY_ROUTING_AGENT,
+            AppConfig.TAG_PROXY
+        )
+        loadDataToPreference(
+            AppConfig.PREF_V2RAY_ROUTING_DIRECT,
+            AppConfig.TAG_DIRECT
+        )
+        loadDataToPreference(
+            AppConfig.PREF_V2RAY_ROUTING_BLOCKED,
+            AppConfig.TAG_BLOCKED
+        )
     }
 
     override fun getWorkManagerConfiguration(): Configuration {
         return Configuration.Builder()
             .setDefaultProcessName("${BuildConfig.APPLICATION_ID}:bg")
             .build()
+    }
+    private fun loadDataToPreference(
+        pref: String,
+        tag: String
+    ) {
+        val content = Utils.readTextFromAssets(this, "custom_routing_$tag")
+        settingsStorage.putString(pref,content).apply()
     }
 }
